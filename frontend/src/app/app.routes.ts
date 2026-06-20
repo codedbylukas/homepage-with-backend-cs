accd15ef709f0a15bd4e16e55e8b3bcf42e1e6cf
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-06-20 06:37:58 +0200

    adding license component more

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { License } from './license/license';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'license', component: License },
+];
