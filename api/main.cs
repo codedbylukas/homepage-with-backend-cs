2911ed552ce9f580560325ede2e44d4c8da38c8c
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-06-28 08:17:27 +0200

    adding good backed to connect fronted and backend

--- api/main.cs
+++ api/main.cs
@@ -13,8 +13,19 @@ builder.Host.UseSerilog();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,7 +33,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowFrontend");
 
+app.UseAuthorization();
+app.MapControllers();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
