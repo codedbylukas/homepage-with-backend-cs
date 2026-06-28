2b7a28d9adbc6498ec755f90558e43f1387f8c1b
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-06-28 08:39:02 +0200

    adding more tests

--- frontend/src/app/number-gessing-game/number-gessing-game.spec.ts
+++ frontend/src/app/number-gessing-game/number-gessing-game.spec.ts
@@ -1,10 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NumberGessingGame } from './number-gessing-game';
 import { render, screen } from '@testing-library/angular';
+import { NumberGessingGame } from './number-gessing-game';
 
 describe('NumberGessingGame', () => {
-  it('should create', () => {
-    expect(NumberGessingGame).toBeTruthy();
+  it('should create', async () => {
+    const { fixture } = await render(NumberGessingGame);
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('defined function load new number', async () => {
+    const { fixture } = await render(NumberGessingGame);
+    const component = fixture.componentInstance;
+    expect(component.loadNewNumber).toBeDefined();
+  });
+  it('defined function check guess', async () => {
+    const { fixture } = await render(NumberGessingGame);
+    const component = fixture.componentInstance;
+    expect(component.checkGuess).toBeDefined();
+  });
+  it('defined function reset game', async () => {
+    const { fixture } = await render(NumberGessingGame);
+    const component = fixture.componentInstance;
+    expect(component.resetGame).toBeDefined();
+  });
+  it('should create shopping list link text', async () => {
+    await render(NumberGessingGame);
+    expect(screen.findAllByText('Einkaufsliste')).toBeTruthy();
   });
 });
