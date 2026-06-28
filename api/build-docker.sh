c8a4d7689bc24508221d4c4978ffe6d0b9975ed8
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-06-28 10:07:09 +0200

    adding

--- api/build-docker.sh
+++ api/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo docker build -t api-homepage .
-sudo docker run -itd -p 5130:5130 --rm --name my-api-homepage api-homepage
+sudo docker run -itd -p 5202:5202 --rm --name my-api-homepage api-homepage
 sudo docker save api-homepage > api-homepage.tar
