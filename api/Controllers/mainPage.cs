5cbb3cc757b0158504ad96681766b96a45b3ba78
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-07-16 19:06:16 +0200

    adding index controller rename

--- api/Controllers/mainPage.cs
+++ api/Controllers/mainPage.cs
@@ -5,10 +5,10 @@ namespace api.Controllers
 {
     [ApiController]
     [Route("/")]
-    public class MainPageController: BaseApiController
+    public class IndexController: BaseApiController
     {
         [HttpGet]
-        public IActionResult MainPageActionResult()
+        public IActionResult Index()
         {
             LogRequest("/");
             string Text = "Diese API ist dazu gemacht, auf die Anfragen des Frontends zu reagieren und nicht um es einfach so im Webbrowser zu nutzen. Dies ist eine reine Empfehlung und keine Verplichtung. Sonst werden Sie warscheinlich so keinen Spaß haben.";
