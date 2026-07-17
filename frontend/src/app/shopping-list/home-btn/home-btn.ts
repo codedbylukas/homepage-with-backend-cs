99b6065b42fe6767eb421216dab48ebdab1bb895
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-07-17 19:03:41 +0200

    starting home-btn in shopping list componen

---
+++ frontend/src/app/shopping-list/home-btn/home-btn.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-btn',
+  imports: [],
+  templateUrl: './home-btn.html',
+  styleUrl: './home-btn.scss',
+})
+export class HomeBtn {}
