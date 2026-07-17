546dae3637685c4408516a6addd2b4ca67725890
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-07-17 20:42:33 +0200

    Revert "Adding more in shopping list"

--- frontend/src/app/shopping-list/shopping-list.spec.ts
+++ frontend/src/app/shopping-list/shopping-list.spec.ts
@@ -1,9 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingList } from './shopping-list';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ShoppingList', () => {
   let component: ShoppingList;
@@ -12,18 +9,10 @@ describe('ShoppingList', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ShoppingList],
-      providers: [provideRouter([]), provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ShoppingList);
     component = fixture.componentInstance;
-
-    // Für Http-mocking
-    fixture.detectChanges();
-    const httpMock = TestBed.inject(HttpTestingController);
-    httpMock.expectOne(() => true).flush([]);
-    // bis hier für http-mocking
-
     await fixture.whenStable();
   });
 
--- frontend/src/app/shopping-list/shopping-list.ts
+++ frontend/src/app/shopping-list/shopping-list.ts
@@ -2,61 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApIModule } from '../api-endpints';
-import { HomeBtn } from './home-btn/home-btn';
 
 @Component({
   selector: 'app-shopping-list',
-  imports: [CommonModule, HomeBtn],
+  imports: [CommonModule], // <-- Hinzugefügt
   templateUrl: './shopping-list.html',
   styleUrl: './shopping-list.scss',
 })
 export class ShoppingList implements OnInit {
-  itemName: string | null = '';
+  second_title: string = 'Füge etwas hinzu in der Einkaufsliste';
+
   private http = inject(HttpClient);
   private cdr = inject(ChangeDetectorRef);
 
-  items: any[] = [];
+  items: any[] = []; // <-- Von "data" zu "items" geändert
   apiEndpoint: string = ApIModule.getApiEndpointShoppingListGet();
 
   ngOnInit(): void {
-    this.loadNewItems();
+    this.loadNewNumber();
   }
 
-  loadNewItems(): void {
+  loadNewNumber(): void {
     this.http.get<any[]>(this.apiEndpoint).subscribe({
       next: (response) => {
-        this.items = response;
+        this.items = response; // <-- Hier "items" befüllen
         console.log('API-Daten erfolgreich geladen:', this.items);
         this.cdr.detectChanges();
       },
       error: (err) => {
         console.error('Fehler beim Laden der API:', err);
-      },
-    });
-  }
-  addItem(): void {
-    this.itemName = prompt('Welches Item möchtest du hinzufügen? (Name einfügen): ');
-    if (this.itemName === null) {
-      return;
-    }
-    this.http.post(`${this.apiEndpoint}?name=${this.itemName}`, {}).subscribe({
-      next: (response) => {
-        console.log('Item erfolgreich hinzugefügt: ', response);
-        this.loadNewItems();
-      },
-      error: (err) => {
-        console.error('Fehler beim Hinzufügen des Items:', err);
-      },
-    });
-  }
-  deleteItem(itemId: number): void {
-    this.http.delete(`${this.apiEndpoint}/${itemId}`, {}).subscribe({
-      next: (response) => {
-        console.log('Item erfolgreich gelöscht: ', itemId);
-        this.loadNewItems();
-      },
-      error: (err) => {
-        console.error('Fehler beim löschen des objektes', err);
+        setTimeout(() => {
+          window.location.reload();
+        }, 500);
       },
     });
   }
