ea1576d687fbdae57397e235fb8381e74ba48bb4
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-07-27 15:14:35 -0500

    adding the first encoding html webpage part

---
+++ frontend/src/app/encoding/encoding.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Encoding } from './encoding';
+
+describe('Encoding', () => {
+  let component: Encoding;
+  let fixture: ComponentFixture<Encoding>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Encoding],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Encoding);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/encoding/encoding.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encoding',
+  imports: [],
+  templateUrl: './encoding.html',
+  styleUrl: './encoding.scss',
+})
+export class Encoding {}
