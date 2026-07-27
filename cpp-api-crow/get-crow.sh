40cf96a20611dc4e9fc81114a460fa4b66505025
codedbylukas <205667645+codedbylukas@users.noreply.github.com>
2026-07-27 11:38:13 -0500

    adding Docker in the .cpp part

--- cpp-api-crow/get-crow.sh
+++ cpp-api-crow/get-crow.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e
+
 git clone --depth 1 https://github.com/CrowCpp/Crow.git /tmp/crow
-cp -r /tmp/crow/include/* include/SSS
-sudo apt update && sudo apt install -y libasio-dev
+mkdir -p include
+cp -r /tmp/crow/include/* include/
+rm -rf /tmp/crow
